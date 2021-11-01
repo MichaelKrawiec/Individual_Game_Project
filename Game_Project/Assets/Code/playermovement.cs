7be2939f2d9948696a601f07c20ade59d1c0efe5
MichaelKrawiec <xxpapu37xx@gmail.com>
2021-11-01 10:52:48 +0000

    Implemented mouse follow movement
    Also included a trail and placeholder background and sprites

---
+++ Game_Project/Assets/Code/playermovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playermovement : MonoBehaviour
+{
+
+    public Rigidbody2D rb;
+    public float turnSpeed = 45;
+    public float moveSpeed = 5;
+
+    void FixedUpdate()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        Vector3 direction = mousePosition - transform.position;
+        float angle = Vector2.SignedAngle(Vector2.right, direction);
+        Vector3 targetRotation = new Vector3(0, 0, angle);
+
+        rb.MoveRotation(Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(targetRotation), turnSpeed * Time.deltaTime));
+        rb.MovePosition(rb.position + ((Vector2)transform.right * moveSpeed * Time.deltaTime));
+    }
+}

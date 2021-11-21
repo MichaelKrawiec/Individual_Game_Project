591ae757245f5219eef007a540fb840855a85637
Michael Krawiec <xxpapu37xx@gmail.com>
2021-11-21 13:55:48 +0000

    Created Level_Exit Object

--- Game_Project/Assets/Code/Rotate_Object.cs
+++ Game_Project/Assets/Code/Rotate_Object.cs
@@ -2,17 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Node : MonoBehaviour
+public class Rotate_Object : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(0, 0, 60 * Time.deltaTime); //rotates 80 degrees per second around z axis
     }
 }
